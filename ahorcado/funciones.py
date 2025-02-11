ede40ec016c43b5769069637f88723d137c8aadd
Uziel ツ <iuzieltsu@gmail.com>
2025-02-11 10:54:29 -0700

    Agregué el archivo de textos a ./datos y desplegamos la plantilla.

--- ahorcado/funciones.py
+++ ahorcado/funciones.py
@@ -7,11 +7,32 @@ def carga_texto_archivo(archivo:str)->list:
     Carga un archivo de texto y devuelve una lista con las
     oraciones del archivo
     '''
-    with open(archivo, 'r') as file:
+    with open(archivo, 'r', encoding='utf-8') as file:
         oraciones = file.readlines()
     return oraciones
 
+def carga_plantillas(nombre_plantilla)->dict:
+    '''
+    Carga las plantillas del juego a partir de un archivo de texto
+    '''
+    plantillas = {}
+    for i in range(5):
+        plantillas[i] = carga_texto_archivo(f'./plantillas/{nombre_plantilla}-{i}.txt')
+    return plantillas
+
+def despliega_plantilla(diccionario:dict, nivel:int):
+    '''
+    Despliega una plantilla del juego
+    '''
+    if nivel >= 0 and nivel <= 5:
+        template = diccionario[nivel]
+        for renglon in template:
+            print(renglon)
+
 if __name__ == '__main__':
-    lista = carga_texto_archivo('./plantillas/plantilla-0.txt')
-    for elemento in lista:
-        print(elemento)
+    plantilla = carga_plantillas('plantilla')
+    despliega_plantilla(plantilla, 4)
+    lista_oraciones = carga_texto_archivo('./datos/pg15532.txt')
+    print(lista_oraciones[110:115])
+    texto = "".join(lista_oraciones[110:])
+    print(texto[:100])
