1f7933f0b79a5770766a7b6c6202f648be58b096
Uziel ツ <iuzieltsu@gmail.com>
2025-02-18 10:52:19 -0700

    Se creo el directorio y el archivo.

---
+++ games/Athlete.py
@@ -0,0 +1,19 @@
+class Athlete:
+    """Athlete class with only name attribute"""
+    def __init__(self, name:str):
+        self.name = name
+
+    def __str__(self):
+        return f"Athlete: {self.name}"
+
+    def __repr__(self):
+        return f"Athlete({self.name})"
+    
+    def display(self):
+        print(f"{self.name}")
+    
+if __name__ == "__main__":
+    a = Athlete("Ana G.")
+    a.display()
+    print(a)
+    print(repr(a))
