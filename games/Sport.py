36f04a3d6bb491fc0ed77a0b82e17a5c8895128e
Uziel ツ <iuzieltsu@gmail.com>
2025-02-19 10:47:19 -0700

    Agregamos la clase Sport.

---
+++ games/Sport.py
@@ -0,0 +1,30 @@
+class Sport:
+    """ Clase para representar un deporte """
+    def __init__(self, name:str, players:int, league:str):
+        self.name = name
+        if isinstance(players, int):
+            self.players = players
+        else:
+            self.players = int(players)
+        self.league = league
+
+    def __str__(self)->str:
+        """ Representación en string de Sport """
+        return f"Sport: {self.name}, {self.players}, {self.league}"
+    
+    def __repr__(self)->str:
+        return f"Sport(name='{self.name}', players={self.players}, league='{self.league}')"
+    
+    def to_json(self)->dict:
+        """ Convertir Sport a JSON"""
+        return {"name":self.name, "players":self.players, "league":self.league}
+    
+if __name__ == "__main__":
+    nfl = Sport("Football", 11, "NFL")
+    print(nfl)
+    print(repr(nfl))
+    print(nfl.to_json)
+    lmp = Sport("Baseball", "9", "LMP")
+    print(lmp)
+    print(repr(lmp))
+    print(lmp.to_json)
