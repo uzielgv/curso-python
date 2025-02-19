fd80da6321166a6d3f763b37c6118360ac5d1402
Uziel ツ <iuzieltsu@gmail.com>
2025-02-19 10:29:26 -0700

    Pequeños cambios al main.

--- games/Athlete.py
+++ games/Athlete.py
@@ -7,7 +7,7 @@ class Athlete:
         return f"Athlete: {self.name}"
 
     def __repr__(self):
-        return f"Athlete({self.name})"
+        return f"Athlete('{self.name}')"
     
     def display(self):
         print(f"{self.name}")
@@ -17,3 +17,9 @@ if __name__ == "__main__":
     a.display()
     print(a)
     print(repr(a))
+    print(f"a: {id(a)}")
+    b = repr(a)
+    print(b)
+    b = eval(b)
+    print(b)
+    print(f"b: {id(b)}")
