20c6de5aa9052142dc20b42fc4c9262683e96dcd
Uziel ツ <iuzieltsu@gmail.com>
2025-02-20 10:54:38 -0700

    Actualización de la clase Sport y creación de la clase Team

--- games/Sport.py
+++ games/Sport.py
@@ -18,17 +18,38 @@ class Sport:
         return f"Sport(name='{self.name}', players={self.players}, league='{self.league}')"
     
     def to_json(self)->dict:
-        """ Convertir Sport a JSON"""
+        """ Convertir Sport a JSON """
         return {"name":self.name, "players":self.players, "league":self.league}
     
 if __name__ == "__main__":
-    nfl = Sport("Football", 11, "NFL")
-    print(nfl)
-    print(repr(nfl))
-    print(nfl.to_json)
-    lmp = Sport("Baseball", "9", "LMP")
-    print(lmp)
-    print(repr(lmp))
-    print(lmp.to_json)
-    lmp2 = eval(repr(lmp))
-    print(lmp2)
+    s = Sport("Soccer", 11, "FIFA")
+    print(s)
+    print(repr(s))
+    print(s.to_json)
+    nfl = Sport("Football", "11", "NFL")
+    lmp = Sport("Baseball", 9, "LMP")
+    mlb = Sport("Baseball", 9, "MLB")
+    lmx = Sport("Soccer", 11, "Liga MX")
+    nba = Sport("Basketball", 5, "NBA")
+    lista_deportes = {nfl, lmp, mlb, lmx, nba, s}
+    archivo_deportes = "deportes.txt"
+    with open(archivo_deportes, "w") as file:
+        for d in lista_deportes:
+            file.write(repr(d)+"\n")
+    sports_list = []
+    with open(archivo_deportes, "r") as file:
+        for line in file:
+            d = eval(line)
+            sports_list.append(d)
+    print(sports_list)
+    print(sports_list[0].to_json())
+
+    import json
+    archivo_json = "deportes.json"
+    sports_json = [sport.to_json() for sport in sports_list]
+    with open(archivo_json, "w") as file:
+        json.dump(sports_json, file, indent=4)
+    sports_list_json = []
+    with open(archivo_json, "r") as file:
+        sports_list_json = json.load(file)
+    print(sports_list_json)
