2dbb4137703a235c5d73b033fb0fbe86586b16a4
Uziel ツ <iuzieltsu@gmail.com>
2025-03-05 10:41:28 -0700

    Indentación del código HTML.

--- hello_world/app.py
+++ hello_world/app.py
@@ -4,11 +4,29 @@ app = Flask(__name__)
 
 @app.route("/") # Home or root of website
 def index():
-    return '<html><head><title>HELLO WORLD</title></head><body><h1>Hello world</h1><p>Ir a <a href="/about">about</a></p></body></html>'
+    ''' Función que se ejecuta cuando se accede a la raíz de la página '''
+    return '''<html>
+                <head>
+                    <title>HELLO WORLD</title>
+                </head>
+                <body>
+                    <h1>Hello world</h1>
+                    <p>Ir a <a href="/about">about</a></p>
+                </body>
+              </html>'''
 
 @app.route("/about") # Info about this site
 def about():
-    return '<html><head><title>About this page</title></head><body>Everything about this website. Back to <a href="/">Hello world</a></body></html>'
+    ''' Función que se ejecuta cuando se accede a la página Acerca de '''
+    return '''<html>
+                <head>
+                    <title>About this page</title>
+                </head>
+                <body>
+                    <h1>Acerca de</h1>
+                    <p>Everything about this website. Back to <a href="/">Hello world</a></p>
+                </body>
+              </html>'''
 
 if __name__ == "__main__":
     app.run(debug=True)
