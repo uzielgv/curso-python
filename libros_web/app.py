a9172bc0929a7d2501c16a9bff6176067d2a6526
Uziel ツ <iuzieltsu@gmail.com>
2025-03-11 10:44:01 -0700

    Ya funciona búsqueda por título.

--- libros_web/app.py
+++ libros_web/app.py
@@ -8,6 +8,7 @@ archivo_csv = "booklist2000.csv"
 lista_libros = fn.lee_archivo_csv(archivo_csv)
 diccionario_titulos = fn.crea_diccionario(lista_libros, 'title')
 diccionario_autores = fn.crea_diccionario(lista_libros, 'author')
+diccionario_id = fn.crea_diccionario(lista_libros, 'id')
 
 @app.route('/')
 def inicio():
@@ -22,6 +23,15 @@ def busqueda_titulo():
         titulo = request.form['titulo']
         resultado = fn.busca_en_diccionario(diccionario_titulos, titulo)
     return render_template('titulo.html', lista_libros=resultado)
+
+@app.route('/libro/<id_libro>', methods=['GET'])
+def libro(id_libro:str):
+    ''' Página de información de un libro '''
+    if id_libro in diccionario_id:
+        book = diccionario_id[id_libro]
+        return render_template('libro.html', libro=book)
+    else:
+        return render_template('libro.html', libro=None)
 
 if __name__ == "__main__":
     app.run(debug=True)
